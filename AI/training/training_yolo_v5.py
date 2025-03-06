a545fc08f25f292e62d3967a55391b1a6cda1405
klei.nagode <klei.nagode@gmail.com>
2025-03-05 20:20:29 -0600

    tried training the model which did not work

--- AI/training/training_yolo_v5.py
+++ AI/training/training_yolo_v5.py
@@ -1,10 +1,17 @@
+# Does not work
 
-from roboflow import Roboflow
-from api_key import Auth
-API_KEY = Auth().key
 
-rf = Roboflow(api_key=API_KEY)
-project = rf.workspace("kyles-workspace").project("basketball-players-detection-flths")
-version = project.version(2)
-dataset = version.download("yolov5")
-                
+from yolov5 import train
+
+# Location to the dataset
+dataset_location =  "C:\\Users\\klein\\Documents\\GitHub\\BoltAthletics\\AI\\training\\basketball-players-detection-2"
+
+# Train the model
+train.run(
+    task='detect',
+    mode='train',
+    model='yolov5x.pt',
+    data=f'{dataset_location}\\data.yaml',
+    epochs=100,
+    imgsz=640
+)
