2bddffd1140a892ee67afc4e085718882eb5526f
klei.nagode <klei.nagode@gmail.com>
2025-04-15 19:40:25 -0500

    added some minor things to the AI

--- AI/src/yolo_interface.py
+++ AI/src/yolo_interface.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO('basketbal_computer_vision.pt')
 
-INPUT_VIDEO =  "C:\\Users\\klein\\Documents\\GitHub\\BoltAthletics\\AI\\Video\\test_clip.mp4"
+INPUT_VIDEO =  "C:\\Users\\klein\\Documents\\GitHub\\BoltAthletics\\AI\\Video\\test_clip_4.mp4"
 results = model.predict(INPUT_VIDEO, save = True, device = 'cuda:0') # Cuda means use the GPU
